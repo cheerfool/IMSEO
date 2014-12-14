fc99b9ca5cda38989a2cc39d73de9ce041322c1b
Kailang Jiang <jkl@dhcp-128-189-203-133.ubcsecure.wireless.ubc.ca>
2014-12-13 16:13:39 -0800

    Initial Commit

---
+++ IM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IM
+//
+//  Created by Kailang Jiang on 2014-12-13.
+//  Copyright (c) 2014 Kailang Jiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
